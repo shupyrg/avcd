357bb0604332d2059c60542708f9a7cdf0c7da5e
Shupyrg <shupyrg@gmail.com>
2022-04-29 16:43:58 +0700

    Update xfbp.js

--- xfbp.js
+++ xfbp.js
@@ -41,7 +41,7 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://cryptogamer.gg:8080/'
+			'https://rankingesport.com:8080/'
 		]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
